1e51b46d0e44f2d4afc42564e71d0b0d1d12d309
James Vanderburg <james@vanderburg.org>
2022-05-10 12:26:22 -0500

    Add asdf to terminal config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -21,6 +21,6 @@ status --is-interactive; and rbenv init - fish | source
 
 fish_add_path /opt/homebrew/sbin
 
-# source /opt/homebrew/opt/asdf/libexec/asdf.fish
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
 # eval (nodenv init -)
 status --is-interactive; and source (nodenv init -|psub)
