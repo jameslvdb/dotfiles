06ca0daa62cde7a2992c1fd9aff741de9e4a0ff8
James Vanderburg <james@vanderburg.org>
2022-07-14 10:21:50 -0500

    Some org-roam change?

--- emacs/.config/emacs/doom/.doom.d/custom.el
+++ emacs/.config/emacs/doom/.doom.d/custom.el
@@ -3,6 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ignored-local-variable-values
+   '((eval setq-local org-roam-directory
+           (expand-file-name "./"))))
  '(package-selected-packages '(ace-jump-mode hotfuzz))
  '(safe-local-variable-values
    '((eval setq-local org-roam-directory
