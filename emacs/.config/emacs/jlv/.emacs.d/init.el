2ffc2c72a65e507d90545c9ebcc6425cffd26fcc
James Vanderburg <james@vanderburg.org>
2022-07-26 20:24:03 -0500

    Add jlv emacs config

---
+++ emacs/.config/emacs/jlv/.emacs.d/init.el
@@ -0,0 +1,51 @@
+;; Don't show the splash screen
+(setq inhibit-startup-message t)
+(setq visible-bell t)
+
+;; Turn off some unneeded UI elements
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+;; Display line numbers in every buffer
+(global-display-line-numbers-mode 1)
+
+;; Load the Modus Vivendi dark theme
+(load-theme 'modus-vivendi t)
+
+(set-face-attribute 'default nil :font "JetBrainsMono Nerd Font" :height 140)
+
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(package-refresh-contents)
+;;(unless package-archive-contents
+;;  (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package doom-modeline
+	     :ensure t
+	     :init (doom-modeline-mode 1)
+	     :custom ((doom-modeline-height 15)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(doom-modeline use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
