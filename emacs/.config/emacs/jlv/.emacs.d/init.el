a88fb0b8ea3f6db36288fb5b5b019491818c7436
James Vanderburg <james@vanderburg.org>
2023-02-09 09:07:51 -0600

    Add UI packages
    - rainbow-delimiters
    - which-key
    - doom-one theme

--- emacs/.config/emacs/jlv/.emacs.d/init.el
+++ emacs/.config/emacs/jlv/.emacs.d/init.el
@@ -12,8 +12,6 @@
 
 (set-face-attribute 'default nil :font "JetBrainsMono Nerd Font" :height 140)
 
-(load-theme 'wombat)
-
 ;; Initialize package sources
 (require 'package)
 
@@ -31,6 +29,9 @@
 
 (require 'use-package)
 (setq use-package-always-ensure t)
+
+(column-number-mode)
+(global-display-line-numbers-mode t)
 
 (use-package ivy
   :diminish
@@ -54,3 +55,36 @@
   :ensure t
   :init (doom-modeline-mode 1))
 
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package which-key
+  :init (which-key-mode))
+
+(use-package doom-themes
+  :ensure t
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-one t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(which-key use-package rainbow-delimiters doom-themes doom-modeline counsel command-log-mode all-the-icons)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
