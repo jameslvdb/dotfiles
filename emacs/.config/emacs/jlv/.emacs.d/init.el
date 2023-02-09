53de1a9e245f450c8377ad8be739ff1820eff358
James Vanderburg <james@vanderburg.org>
2023-02-08 18:30:23 -0600

    Start use-package setup

--- emacs/.config/emacs/jlv/.emacs.d/init.el
+++ emacs/.config/emacs/jlv/.emacs.d/init.el
@@ -13,3 +13,21 @@
 (set-face-attribute 'default nil :font "JetBrainsMono Nerd Font" :height 140)
 
 (load-theme 'tango-dark)
+
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
