e307cdba441c07b43f66c628bfd5e41dd522c970
James Vanderburg <james@vanderburg.org>
2023-02-08 18:23:37 -0600

    Strip down personal emacs config

--- emacs/.config/emacs/jlv/.emacs.d/init.el
+++ emacs/.config/emacs/jlv/.emacs.d/init.el
@@ -1,101 +1,15 @@
-;; Don't show the splash screen
 (setq inhibit-startup-message t)
 
-;; Turn off some unneeded UI elements
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
+(scroll-bar-mode -1) ; Disable visible scrollbar
+(tool-bar-mode -1) ; Disable the toolbar
+(tooltip-mode -1) ; Disable tooltips
+(set-fringe-mode 10) ; Give some breathing room
 
-;; Display line numbers in every buffer
-(global-display-line-numbers-mode 1)
+(menu-bar-mode -1)
 
-;; Make ESC quit prompts
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+;; Set up the visible bell
+(setq visible-bell t)
 
 (set-face-attribute 'default nil :font "JetBrainsMono Nerd Font" :height 140)
 
-;; Initialize package sources
-(require 'package)
-
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			 ("org" . "https://orgmode.org/elpa/")
-			 ("elpa" . "https://elpa.gnu.org/packages/")))
-
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
-
-;; Initialize use-package on non-Linux platforms
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-
-(require 'use-package)
-(setq use-package-always-ensure t)
-
-(use-package command-log-mode)
-
-(use-package doom-themes
-  :ensure t
-  :config
-  ;; Global settings (defaults)
-  (setq doom-themes-enable-bold t     ; if nil, bold is universally disabled
-	doom-themes-enable-italic t)  ; if nil, italics are universally disabled
-  (load-theme 'doom-one t)
-
-  ;; Enable flashing modeline on errors
-  (doom-themes-visual-bell-config)
-  ;; Corrects and improves org-mode's native fontification
-  (doom-themes-org-config))
-
-(use-package doom-modeline
-	     :ensure t
-	     :init (doom-modeline-mode 1))
-
-(use-package all-the-icons
-  :if (display-graphic-p))
-
-(use-package swiper)
-
-(use-package ivy
-  :diminish
-  :bind (("C-s" . swiper)
-	 :map ivy-minibuffer-map
-	 ("TAB" . ivy-alt-done)
-	 ("C-l" . ivy-alt-done)
-	 ("C-j" . ivy-next-line)
-	 ("C-k" . ivy-previous-line)
-	 :map ivy-switch-buffer-map
-	 ("C-k" . ivy-previous-line)
-	 ("C-l" . ivy-done)
-	 ("C-d" . ivy-switch-buffer-kill)
-	 :map ivy-reverse-i-search-map
-	 ("C-k" . ivy-previous-line)
-	 ("C-d" . ivy-reverse-i-search-kill))
-  :config
-  (ivy-mode 1))
-
-(use-package counsel
-  :bind (("M-x" . counsel-M-x)
-	 ("C-x b" . counsel-ibuffer)
-	 ("C-x C-f" . counsel-find-file)
-	 :map minibuffer-local-map
-	 ("C-r" . 'counsel-minibuffer-history))
-  :config
-  (setq ivy-initial-inputs-alist nil)) ;; Don't start searches with ^
-
-
-
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(doom-themes all-the-icons counsel swiper ivy command-log-mode doom-modeline use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(load-theme 'tango-dark)
