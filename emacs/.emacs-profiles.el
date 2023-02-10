62a2220fe59b200fd70751bf16efee311a4ab944
James Vanderburg <james@vanderburg.org>
2023-02-10 11:32:35 -0600

    Remove emacs configs

--- emacs/.emacs-profiles.el
+++
@@ -1,6 +0,0 @@
-(("spacemacs" . ((user-emacs-directory . "~/.config/emacs/spacemacs/.emacs.d")))
- ("prelude"   . ((user-emacs-directory . "~/.config/emacs/prelude/.emacs.d")))
- ("jlv"       . ((user-emacs-directory . "~/.config/emacs/jlv/.emacs.d")))
- ("default"   . ((user-emacs-directory . "~/.config/emacs/doom/.emacs.d")
-                 (env . (("EMACSDIR"   . "~/.config/emacs/doom/.emacs.d")
-		                 ("DOOMDIR"    . "~/.config/emacs/doom/.doom.d"))))))
