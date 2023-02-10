20f19d5cceb473824717ebf0e50682696abab812
James Vanderburg <james@vanderburg.org>
2023-02-10 13:18:38 -0600

    Remove boilerplate

--- doom/.doom.d/ruby.el
+++ doom/.doom.d/ruby.el
@@ -7,15 +7,6 @@
 ;; Created: February 10, 2023
 ;; Modified: February 10, 2023
 ;; Version: 0.0.1
-;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
-;; Homepage: https://github.com/jameslvdb/ruby
-;; Package-Requires: ((emacs "24.3"))
-;;
-;; This file is not part of GNU Emacs.
-;;
-;;; Commentary:
-;;
-;;
 ;;
 ;;; Code:
 
