dafab58f43c3d67945c121aeae4b7a6d8c15a76b
James Vanderburg <james@vanderburg.org>
2023-03-02 13:46:29 -0600

    Add smartparens curly brace handling

--- doom/.doom.d/lisp/ruby.el
+++ doom/.doom.d/lisp/ruby.el
@@ -11,3 +11,13 @@
     (shell-command (format "standardrb --fix %s" buffer-file-name))))
 
 (add-hook 'after-save-hook #'format-ruby-on-save)
+
+;; Brace management
+(sp-local-pair 'ruby-mode "{" nil :post-handlers '((my-create-newline-and-enter-sexp "RET")))
+
+(defun my-create-newline-and-enter-sexp (&rest _ignored)
+  "Open a new brace or bracket expression, with relevant newlines and indent. "
+  (newline)
+  (indent-according-to-mode)
+  (forward-line -1)
+  (indent-according-to-mode))
