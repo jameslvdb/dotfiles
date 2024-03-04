6e9a12e1ed3e9488ef172deef50c2429172014ce
James Vanderburg <james@vanderburg.org>
2024-03-04 10:25:11 -0600

    Update fish config with new aliases

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,18 +2,20 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-alias ls="exa"
-alias l="exa -lah --git"
+alias ls="eza"
+alias l="eza -lah --git"
 alias cat="bat"
 
 # cd-to-repo aliases
 alias backend="cd ~/src/pgahq/coach-tools-backend"
 alias frontend="cd ~/src/pgahq/coaching-programs-frontend"
 alias mobile="cd ~/src/pgahq/coach-tools-mobile-frontend"
-alias redesign="cd ~/src/pgahq/pga-site-redesign"
+alias dotcom="cd ~/src/pgahq/www-pga-com/"
+alias pgacom="dotcom"
+alias obsidian="cd ~/src/jameslvdb/obsidian"
 
 # edit config file aliases
-alias fishconfig="vim ~/.config/fish/config.fish"
+alias fishconfig="nvim ~/.config/fish/config.fish"
 
 # Rails-specific aliases
 alias be="bundle exec"
@@ -29,9 +31,20 @@ alias looker="open https://pgahq.cloud.looker.com/sql"
 alias python="python3"
 alias pip="pip3"
 
+# gcc alias 
+alias gcc="gcc-13"
+
+# Use FVM for all Dart/Flutter commands
+alias flutter="fvm flutter"
+alias dart"fvm dart"
+
 starship init fish | source
 status --is-interactive; and rbenv init - fish | source
 
 fish_add_path /opt/homebrew/sbin
+fish_add_path ~/src/flutter/bin
+
+set PATH ~/.config/emacs/bin/ $PATH
+# set PATH /usr/local/bin
 
 source /opt/homebrew/opt/asdf/libexec/asdf.fish
