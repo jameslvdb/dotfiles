b31fa2b66bbea68db472f82284e025b565f1bb3a
James Vanderburg <james@vanderburg.org>
2025-02-26 10:32:33 -0600

    Update fish aliases

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,6 +9,7 @@ alias cat="bat"
 # cd-to-repo aliases
 alias backend="cd ~/src/pgahq/coach-tools-backend"
 alias frontend="cd ~/src/pgahq/coaching-programs-frontend"
+alias backoffice="cd ~/src/pgahq/backoffice-frontend"
 alias mobile="cd ~/src/pgahq/coach-tools-mobile-frontend"
 alias dotcom="cd ~/src/pgahq/www-pga-com/"
 alias pgacom="dotcom"
@@ -19,6 +20,10 @@ alias fishconfig="nvim ~/.config/fish/config.fish"
 
 # Rails-specific aliases
 alias be="bundle exec"
+
+# Docker aliases
+alias oracle-down="docker-compose down -- oracledb"
+alias oracle-up="docker-compose up -d -- oracledb"
 
 # GQL aliases
 alias dump_gql_schema="rake graphql:schema:idl"
@@ -34,10 +39,6 @@ alias pip="pip3"
 # gcc alias 
 alias gcc="gcc-13"
 
-# Use FVM for all Dart/Flutter commands
-alias flutter="fvm flutter"
-alias dart"fvm dart"
-
 starship init fish | source
 status --is-interactive; and rbenv init - fish | source
 
@@ -48,3 +49,4 @@ set PATH ~/.config/emacs/bin/ $PATH
 # set PATH /usr/local/bin
 
 source /opt/homebrew/opt/asdf/libexec/asdf.fish
+status --is-interactive; and nodenv init - fish | source
