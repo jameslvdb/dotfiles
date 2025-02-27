eb42db1b4b0972fd2e5e9daa17d45004fe1a0d04
James Vanderburg <james@vanderburg.org>
2025-02-27 14:07:41 -0600

    Update render-markdown config to allow experimentation

--- nvim/.config/nvim/lua/plugins/render-markdown.lua
+++ nvim/.config/nvim/lua/plugins/render-markdown.lua
@@ -1,3 +1,45 @@
 return {
-  { "MeanderingProgrammer/render-markdown.nvim", enabled = false },
+  "MeanderingProgrammer/render-markdown.nvim",
+  opts = {
+    anti_conceal = {
+      -- default is `true`
+      enabled = true,
+    },
+    heading = {
+      enabled = false,
+    },
+    paragraph = {
+      enabled = false,
+    },
+    code = {
+      enabled = false,
+    },
+    bullet = {
+      enabled = false,
+    },
+    dash = {
+      enabled = false,
+    },
+    checkbox = {
+      enabled = false,
+    },
+    quote = {
+      enabled = false,
+    },
+    pipe_table = {
+      enabled = false,
+    },
+    callout = {
+      enabled = false,
+    },
+    link = {
+      enabled = false,
+    },
+    sign = {
+      enabled = false,
+    },
+    indent = {
+      enabled = false,
+    },
+  },
 }
