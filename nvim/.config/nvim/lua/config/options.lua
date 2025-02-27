033f1e17b872ef7fe2872ba3f43c158195904c47
James Vanderburg <james@vanderburg.org>
2025-02-27 13:53:48 -0600

    Set conceallevel to 0

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+local opt = vim.opt
+opt.conceallevel = 0
+
 vim.g.lazyvim_ruby_formatter = "standardrb"
 
 vim.api.nvim_create_autocmd("FileType", {
